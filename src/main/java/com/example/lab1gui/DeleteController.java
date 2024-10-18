cc35d584fc051dad8f3f94ba9607f50e43de2aff
Mattazz <matthewazada101@gmail.com>
2024-10-18 10:37:15 -0400

    final update

---
+++ src/main/java/com/example/lab1gui/DeleteController.java
@@ -0,0 +1,29 @@
+package com.example.lab1gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class SelectController {
+
+    public Label selectMessage;
+    public Button searchViewBtn;
+    public Button createViewBtn;
+
+
+    public void goToSearchView(ActionEvent actionEvent) {
+        String scene = "/com/example/lab1gui/search-view.fxml";
+        Stage stage = (Stage) searchViewBtn.getScene().getWindow();
+
+        SceneManager.changeScene(stage, scene, selectMessage );
+    }
+
+    public void goToCreateView(ActionEvent actionEvent) {
+        String scene = "/com/example/lab1gui/create-view.fxml";
+        Stage stage = (Stage) searchViewBtn.getScene().getWindow();
+
+        SceneManager.changeScene(stage, scene, selectMessage );
+    }
+}
---
+++ src/main/java/com/example/lab1gui/UpdateController.java
@@ -0,0 +1,12 @@
+package com.example.lab1gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class DeleteController {
+    public Button deleteBtn;
+    public TextField recordIDInput;
+}
